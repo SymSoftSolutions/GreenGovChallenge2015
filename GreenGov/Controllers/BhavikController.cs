90874d4632931d830044d23e929e2190c13a1717
bhavik-symsoft <bhavik@symsoftsolutions.com>
2015-10-24 18:58:44 -0700

    - Adding more stuff, I really can't remember what anymore, sorry.

---
+++ GreenGov/Controllers/BhavikController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreenGov.Controllers
+{
+    public class BhavikController : Controller
+    {
+        // GET: Bhavik
+        public ActionResult Index(string name)
+        {
+            return View();
+        }
+    }
+}
